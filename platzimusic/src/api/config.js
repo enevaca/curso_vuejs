23aba9feb435ac1f5d3ce6fe83296fe6f3d2634c
Noel Vaca <noel.vm.2501@gmail.com>
2018-03-02 16:47:54 -0400

    Configurando la API de Last.fm en Vue.js

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appName: 'platzimusic',
+	apiKey: '48b12353f41ca284f5a3596b490e3ee8',
+	secret: '8d0f9bf906bad8c36e88eaec6bbe6493',
+	registeredTo: 'noel-vm'
+}
+
+// Application name	platzimusic
+// API key	48b12353f41ca284f5a3596b490e3ee8
+// Shared secret	8d0f9bf906bad8c36e88eaec6bbe6493
+// Registered to	noel-vm
